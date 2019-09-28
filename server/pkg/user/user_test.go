1927975f0c0dd52aba6503e0f4fca5663308eda6
spaceCh1mp <drimboat@gmail.com>
2019-09-28 23:21:33 +0100

    Added tests for the Create user method

--- server/pkg/user/user_test.go
+++ server/pkg/user/user_test.go
@@ -1,9 +1,61 @@
 package user
 
 import (
+	"fmt"
 	"log"
 	"testing"
+
+	context "golang.org/x/net/context"
+
+	v1 "github.com/spaceCh1mp/pow/server/api/proto/v1"
 )
+
+//global interface for usersServer
+var usersServerTest usersServer
+
+func TestCreate(t *testing.T) {
+	// test table containing test cases for the create user method
+	tt := []struct {
+		name string
+		tc   v1.NewUser
+		err  error
+	}{
+		{"Ok", v1.NewUser{
+			FirstName: "Kenechukwu",
+			LastName:  "Agugua",
+			Email:     "Kenechukwuagugua@gmail.com",
+			Password:  "123455788",
+		},
+			nil},
+		{"No FirstName", v1.NewUser{
+			LastName: "Agugua",
+			Email:    "Kenechukwuagugua@gmail.com",
+			Password: "123455788",
+		},
+			fmt.Errorf("Missing Field: FirstName")},
+		{"No LastName", v1.NewUser{
+			FirstName: "Kenechukwu",
+			Email:     "Kenechukwuagugua@gmail.com",
+			Password:  "123455788",
+		},
+			fmt.Errorf("Missing Field: LastName")},
+		{"No Email", v1.NewUser{
+			FirstName: "Kenechukwu",
+			LastName:  "Agugua",
+			Password:  "123455788",
+		},
+			fmt.Errorf("Missing Field: Email")},
+	}
+
+	for _, v := range tt {
+		t.Run(v.name, func(t *testing.T) {
+			_, err := usersServerTest.Create(context.Background(), &v.tc)
+			if err != v.err {
+				t.Fatalf("Expected: %v \n Got: %v", v.err, err)
+			}
+		})
+	}
+}
 
 func TestGenRandString(t *testing.T) {
 	//declare string array for a multiple of five instances
