559e3c15eb1453d551e43f485bbecde570419082
Agugua Kenechukwu <spacech1mp@Aguguas-MacBook-Pro.local>
2020-01-11 07:31:52 +0100

    Implemented Create and ReadTransaction methods for the transaction service

--- server/pkg/transaction/transaction.go
+++ server/pkg/transaction/transaction.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 
+	"go.mongodb.org/mongo-driver/bson"
+
 	v1 "github.com/spaceCh1mp/pow/server/api/proto/v1"
 	"github.com/spaceCh1mp/pow/server/db"
 	grpc "google.golang.org/grpc"
@@ -50,8 +52,36 @@ func Config() error {
 }
 
 func (t transactionsServer) Create(c context.Context, nt *v1.NewTransaction) (*v1.Transaction, error) {
-	return &v1.Transaction{}, nil
+	resp, err := pool.Insert(nt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &v1.Transaction{
+		Id:     resp,
+		Amount: nt.Amount,
+		Userid: nt.Userid,
+		Date:   nt.Date,
+	}, nil
 }
+
 func (t transactionsServer) ReadTransaction(c context.Context, td *v1.ID) (*v1.Transaction, error) {
-	return &v1.Transaction{}, nil
+
+	resp, err := pool.Read([]byte(td.GetId()))
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := bson.Marshal(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var transactionData v1.Transaction
+	err = bson.Unmarshal(b, &transactionData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &transactionData, nil
 }
